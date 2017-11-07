2d70fd57b53b75d77962dc181b2587c859240bfa
Aman Gupta <agupta330@csc.com>
2017-11-07 14:47:48 +0530

    v0.0.2

--- src/ng-user-panel.directive.js
+++ src/ng-user-panel.directive.js
@@ -60,10 +60,95 @@ angular
   }
 }])
 
-.directive('ng1UserPanel', function() {
+.directive('user', function($templateCache) {
+
+  var TEMPLATE_URL_USERPICKER = '/userpicker/index.html';
+
+  // Set the default template for this directive
+  $templateCache.put(TEMPLATE_URL_USERPICKER,
+      '<input type="text" name="user" id="user"  placeholder="User" ng-blur="getUser()" ng-model="userEmail" ng-minlength="1" ng-maxlength="75">'+
+      '<br>'+
+      '<ANY ng-switch="showFlag">'+
+      '<ANY ng-switch-when="user">'+
+          '<div ng-include = "getTemplate()"></div>'+
+       '</ANY>'+
+       '<ANY ng-switch-when="noUser">'+
+          '<div class="text-danger" style="width:180px;!important;">'+
+             'User not found!!!'+
+          '</div>'+
+       '</ANY>'+
+       '<ANY ng-switch-default>'+
+          '<div class="text-info" style="width:180px;!important;">'+
+             '<!-- Default panel -->'+
+          '</div>'+
+       '</ANY>'+
+    '</ANY>'
+  );
+
+  var TEMPLATE_URL_SMALLPANEL ='/userpicker/smallpanel.html';
+  
+  // Set the smallpanel template for this directive
+  $templateCache.put(TEMPLATE_URL_SMALLPANEL,
+    '<br>'+
+    '<div class="panel  panel-default" class="panel-bg" >'+
+      '<div class="panel-body">'+
+        '<div class="pull-left">'+
+          '<img src="bower_components/ng1UserPanel/src/images/default_male300x300-aae6ae0235b6cd78cee8df7ae19f6085.png" class="img-circle userpic">'+
+        '</div>'+
+      '<div class="panelpad">'+
+        '<div class="text-highlight">{{userModel.name.first}} {{userModel.name.last}}</div>'+
+        '<div class="text-xsmall">{{userModel.jobTitle}}</div>'+
+        '<div class="text-xsmall">{{userModel.organization}}</div>'+
+      '</div>'+
+    '</div>'+
+    '</div>'
+  );
+
+  var TEMPLATE_URL_MEDPANEL ='/userpicker/mediumpanel.html';
+  
+  // Set the smallpanel template for this directive
+  $templateCache.put(TEMPLATE_URL_MEDPANEL,
+      '<br>'+
+      '<div class="panel panel-default">'+
+         '<div class="panel-body">'+
+            '<div class="pull-left">'+
+               '<img src="bower_components/ng1UserPanel/src/images/default_male300x300-aae6ae0235b6cd78cee8df7ae19f6085.png" class="img-circle userpicmd"> '+
+            '</div>'+
+            '<div class="panelpdmd">'+
+               '<div class="text-highlight">{{userModel.name.first}} {{userModel.name.last}}</div>'+
+               '<div class="text-xsmall">{{userModel.email}}</div>'+
+               '<div class="text-xsmall">{{userModel.jobTitle}}</div>'+
+               '<div class="text-xsmall">{{userModel.organization}}</div>'+
+            '</div>'+
+         '</div>'+
+      '</div>'+
+      '</div>'
+  );
+
+  var TEMPLATE_URL_LARGEPANEL ='/userpicker/largepanel.html';
+  
+  // Set the smallpanel template for this directive
+  $templateCache.put(TEMPLATE_URL_LARGEPANEL,
+      '<br>'+
+      '<div class="panel panel-default">'+
+         '<div class="panel-body ">'+
+            '<div class="pull-left">'+
+               '<img src="bower_components/ng1UserPanel/src/images/default_male300x300-aae6ae0235b6cd78cee8df7ae19f6085.png" class="img-circle userpiclg"> '+
+            '</div>'+
+            '<div class="panelpdlg">'+
+               '<div class="text-highlight">{{userModel.name.first}} {{userModel.name.last}}</div>'+
+               '<div class="text-xsmall">{{userModel.email}}</div>'+
+               '<div class="text-xsmall">{{userModel.jobTitle}}</div>'+
+               '<div class="text-xsmall">{{userModel.organization}}</div>'+
+               '<div class="text-desc">{{userModel.summary}}</div>'+
+            '</div>'+
+         '</div>'+
+      '</div>'+
+      '</div>'
+  );
   return {
     controller: 'ng1UserPanelControllerMain',
-    templateUrl: 'templates/user-picker.html',
+    // templateUrl: 'templates/user-picker.html',
     scope: {
       userModel: '=userModel',
       userId: '=userId',
@@ -72,7 +157,9 @@ angular
       switchMode: '=switchMode',
       userType: '@userType'
     },
-
+    templateUrl: function(element, attrs) {
+      return attrs.templateUrl || TEMPLATE_URL_USERPICKER;
+    },
     link : function(scope)
     {
       scope.getTemplate = function(){
@@ -81,13 +168,13 @@ angular
 
         if(viewmode === 'small' && scope.userEmail!=='')
         {
-          return 'templates/smallpanel.html';
+          return TEMPLATE_URL_SMALLPANEL;
         }
         if(viewmode === 'large'){
-          return 'templates/largepanel.html';
+          return TEMPLATE_URL_LARGEPANEL;
         }
         if(viewmode === 'medium'){
-          return 'templates/mediumpanel.html';
+          return TEMPLATE_URL_MEDPANEL;
         }
 
       };
